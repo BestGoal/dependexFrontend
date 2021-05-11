67dcf3fd523d5a7bf9d96aa3a380bee6be9c995f
winner108 <73909926+winner108@users.noreply.github.com>
2021-05-11 17:14:47 +0300

    update

--- build/static/js/10.ee7aa44e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdependex-frontend"]=this["webpackJsonpdependex-frontend"]||[]).push([[10],{249:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return d}));var r=t(176),c=(t(0),t(1));function d(){return Object(c.jsx)(r.a,{children:Object(c.jsx)("h1",{children:"Security"})})}}}]);
-//# sourceMappingURL=10.ee7aa44e.chunk.js.map
